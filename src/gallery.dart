ca29abbfa5e891c92d1156f288e0cc3e35768b88
joeltok <jttctc@gmail.com>
2020-10-05 15:43:26 +0800

    Create gallery.dart

---
+++ src/gallery.dart
@@ -0,0 +1,17 @@
+class Gallery extends StatefulWidget {
+  @override
+  _GalleryState createState() => _GalleryState();
+}
+
+class _GalleryState extends State<Gallery> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Gallery'),
+        centerTitle: true,
+      ),
+      body: Container(),
+    );
+  }
+}
